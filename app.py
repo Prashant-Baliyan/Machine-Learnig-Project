51ea971160b51c1943bb21a55e5e9159e57a88c7
Prashant-Baliyan <pbaliyan1992@gmail.com>
2022-06-18 23:40:34 +0530

    Added yaml file along with logger and exception

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.Exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "This is my first machine larning flask app"
 
 if __name__ == "__main__":
